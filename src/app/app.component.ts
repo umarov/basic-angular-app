a16e1a990a845b361cf9d3cbb64c3ea736bf9fef
Muzafar Umarov <umarov@gmail.com>
2018-05-17 22:14:10 -0400

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'basic-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'basic';
+}
